63e78e46340ef7e1a10a57f39bc6be6d223c8053
aaamarkin <aaamarkin@gmail.com>
2019-08-16 20:11:05 +0300

    Trivial multiply method implemented

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,40 @@
-//fn main() {
-//    println!("Hello, world!");
-//}
+use std::num::ParseIntError;
 
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+
+
+fn multiply(a: &str, b: &str) -> Result<String, ParseIntError> {
+    let a_parsed: i32 = a.parse::<i32>()?;
+    let b_parsed: i32 = b.parse::<i32>()?;
+
+    let res = a_parsed * b_parsed;
+    let res_str = res.to_string();
+
+    return Ok(res_str);
 }
-
-// This is a really bad adding function, its purpose is to fail in this
-// example.
-#[allow(dead_code)]
-fn bad_add(a: i32, b: i32) -> i32 {
-    a - b
-}
-
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
-    fn test_add() {
-        assert_eq!(add(1, 2), 3);
+    fn test_mul1() {
+        assert_eq!(multiply("4", "8"), Ok("32".to_string()));
     }
 
     #[test]
-    fn test_bad_add() {
-        // This assert would fire and test will fail.
-        // Please note, that private functions can be tested too!
-        assert_eq!(bad_add(1, 2), 3);
+    fn test_mul2() {
+        assert_eq!(multiply("8", "8"), Ok("64".to_string()));
     }
+}
+
+fn main() {
+    let first_num = "3141592653589793238462643383279502884197169399375105820974944592";
+    let second_num = "2718281828459045235360287471352662497757247093699959574966967627";
+//    let first_num = "3";
+//    let second_num = "2";
+    match multiply(first_num, second_num)  {
+        Ok(a) =>  println!("{}", a),
+        Err(e) => println!("{}", e.to_string()),
+    }
+
 }
